71a14d5682d2bb9d179cb836509a3a4c0b729c3c
javad rajabi <javvadrajabi@gmail.com>
2021-09-22 19:11:17 +0330

    main

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '64943ddc2e25edb94f682492bd0818d5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9660baa4-0870-4c94-851f-81901d2e3718'
+};
